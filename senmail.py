6c2f6583da670e5f29c7157baeb578dc3c45b489
sdayfic <sdayfic@163.com>
2019-05-02 09:11:58 +0800

    this a zabbix sent email script

---
+++ senmail.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from email.mime.text import MIMEText
+from email.header import Header
+from smtplib import SMTP_SSL
+import sys
+sent_smtp = 'smtp.163.com'
+sent_user = 'sdayfic'
+pwd = '800128aa'
+sent_mail = 'sdayfic@163.com'
+rece_mail =sys.argv[1]
+mail_title = sys.argv[2]
+mail_content = sys.argv[3]
+smtp = SMTP_SSL(sent_smtp)
+smtp.set_debuglevel(0)
+smtp.ehlo(sent_smtp)
+smtp.login(sent_user , pwd)
+msg = MIMEText(mail_content , "plain" , 'utf-8')
+msg ["Subject"] = Header(mail_title , 'utf-8')
+msg ["From"] = sent_mail
+msg ["To"] = rece_mail
+smtp.sendmail(sent_mail , rece_mail , msg.as_string())
+smtp.quit()
