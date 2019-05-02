fcf300d670e6f29b8d36911e5a0ad0f66499e146
sdayfic <sdayfic@163.com>
2019-05-02 08:56:31 +0800

    this is a zabbix dingding script

---
+++ dingding.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import requests
+import json
+import sys
+import os
+
+headers = {'Content-Type': 'application/json;charset=utf-8'}
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=1df323ece6938cdf6ea5e25242a1951f8be893609290f2eb34cc002ea4bf7486"
+
+
+def msg(text):
+  json_text = {
+    "msgtype": "text" ,
+    "at": {
+      "atMobiles": [
+        "18018336335"
+      ] ,
+      "isAtAll": False
+    } ,
+    "text": {
+      "content": text
+    }
+  }
+  print requests.post(api_url , json.dumps(json_text) , headers = headers).content
+
+
+if __name__ == '__main__':
+  text = sys.argv [1]
+  msg(text)
